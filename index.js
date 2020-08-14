d778b1533f6dc66b266ac574980b675ed40554a7
matt22881 <47930310+matt22881@users.noreply.github.com>
2020-08-14 17:15:14 -0400

    changing gears, take a break, then polish some css

--- index.js
+++ index.js
@@ -208,11 +208,14 @@ const artists = [
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
-
+console.log(artists[0].name)
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
-
-
+ function changeName(arr, idx, newName){
+   arr.splice(idx, 1, newName)
+ }
+// console.log(changeName(artists, 9, 'Vincent Van Gogh'))
+// console.log(artists[9])
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
@@ -222,20 +225,29 @@ const artists = [
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(array, index) {
-    /* code here */
+function getArtistByIndex(arr, idx) {
+  console.log(`The artist at index ${idx} is ${artists[idx].name}.`)
   }
-  
+  getArtistByIndex(artists, 0)
   /**
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/* Code here */){
-
-  /* Code here */
-
+function get20s(arr) {
+  let newArr = []
+  for (let i=0; i<arr.length; i++){
+    if (arr[i].years.charAt(3, 4) === '9' && arr[i].years.charAt(10, 11) === '9'){
+newArr.push(arr[i]);
+console.log(arr[i]);
 }
+}
+    
+    console.log (newArr);
+  return newArr
+  };
+console.log(get20s(artists));
+  
 
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
